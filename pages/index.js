575e5c93541d64d688601e7d19dd6758ef2233d7
Filipe Alves <lipeqa@gmail.com>
2022-11-09 22:26:19 -0300

    adiciona input de busca aula 02

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import config from "../config.json";
 import styled from "styled-components";
 import { CSSReset } from "../src/componnts/CSSReset";
@@ -5,23 +6,24 @@ import Menu from "../src/componnts/Menu";
 import { StyledTimeline } from "../src/componnts/Timeline";
 
 function HomePage() {
+  const [valorDoFiltro, setValorDoFiltro] = React.useState("Angular");
+
   return (
     <>
       <CSSReset />
       <div>
-        <Menu />
+        <Menu
+          valorDoFiltro={valorDoFiltro}
+          setValorDoFiltro={setValorDoFiltro}
+        />
         <Header />
-        <TimeLine playlists={config.playlists} />
+        <TimeLine searchValue={valorDoFiltro} playlists={config.playlists} />
       </div>
     </>
   );
 }
 
 export default HomePage;
-
-/*function Menu() {
-  return <div>Menu</div>;
-}*/
 
 const StyledHeader = styled.header`
   .img-perfil {
@@ -36,20 +38,29 @@ const StyledHeader = styled.header`
     width: 100%;
     padding: 16px;
   }
-  .img-banner{
+  /*.img-banner {
     width: 100vw;
     height: 230px;
     left: 0px;
     top: 56px;
-  }
+  }*/
+`;
+
+const StyledBanner = styled.div`
+    height: 230px;
+    background-image: url(${({banner}) => banner});
 `;
 
 function Header() {
   return (
     <StyledHeader>
-      {<img className="img-banner" src={config.banner} /> }
+    <StyledBanner banner={config.banner} />
+      {/*{<img className="img-banner" src={config.banner} />}*/}
       <section className="user-info">
-        <img className="img-perfil" src={`https://github.com/${config.github}.png`} />
+        <img
+          className="img-perfil"
+          src={`https://github.com/${config.github}.png`}
+        />
         <div>
           <h2>{config.name}</h2>
           <p> {config.job}</p>
@@ -59,7 +70,7 @@ function Header() {
   );
 }
 
-function TimeLine(props) {
+function TimeLine({ searchValue, ...props }) {
   const playlistNames = Object.keys(props.playlists);
   //Statement
   //Retorno por expressão
@@ -72,14 +83,20 @@ function TimeLine(props) {
           <section>
             <h2>{playlistName}</h2>
             <div>
-              {videos.map((video) => {
-                return (
-                  <a href={video.url}>
-                    <img src={video.thumb} />
-                    <span>{video.title}</span>
-                  </a>
-                );
-              })}
+              {videos
+                .filter((video) => {
+                  const titleNormalzed = video.title.toLowerCase();
+                  const searchValueNormalzed = searchValue.toLowerCase();
+                  return titleNormalzed.includes(searchValueNormalzed);
+                })
+                .map((video) => {
+                  return (
+                    <a href={video.url}>
+                      <img src={video.thumb} />
+                      <span>{video.title}</span>
+                    </a>
+                  );
+                })}
             </div>
           </section>
         );
